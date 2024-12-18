6433a848dd3fdba98b55aa28da6b0514310ce92c
Divi Newton <divinewton@gmail.com>
2024-12-17 18:14:09 -0800

    Basic gallery layout

---
+++ gallery.js
@@ -0,0 +1,7 @@
+/* Mobile Nav Open/Close */
+function openMobileNav() {
+    document.getElementById("mobile-nav-overlay").style.width = "100%";
+}
+function closeMobileNav() {
+    document.getElementById("mobile-nav-overlay").style.width = "0";
+}
