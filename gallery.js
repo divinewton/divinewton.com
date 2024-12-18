f8e1a7afeff15e5920d34557549e8d74b4868b53
Divi Newton <divinewton@gmail.com>
2024-12-17 18:42:53 -0800

    Update js

--- gallery.js
+++ gallery.js
@@ -5,3 +5,59 @@ function openMobileNav() {
 function closeMobileNav() {
     document.getElementById("mobile-nav-overlay").style.width = "0";
 }
+
+/* All Button */
+function showAll() {
+    const elements = document.querySelectorAll("[id^='ui-img'], [id^='visual-img'], [id^='physical-img']");
+    elements.forEach(element => {
+        element.style.display = "block";
+    });
+}
+
+/* User Interface Button */
+function showUI() {
+    const elements = document.querySelectorAll("[id^='ui-img']");
+    elements.forEach(element => {
+        element.style.display = "block";
+    });
+    hideVisual();
+    hidePhysical();
+}
+function hideUI() {
+    const elements = document.querySelectorAll("[id^='ui-img']");
+    elements.forEach(element => {
+        element.style.display = "none";
+    });
+}
+
+/* Visual Art Button */
+function showVisual() {
+    const elements = document.querySelectorAll("[id^='visual-img']");
+    elements.forEach(element => {
+        element.style.display = "block";
+    });
+    hideUI();
+    hidePhysical();
+}
+function hideVisual() {
+    const elements = document.querySelectorAll("[id^='visual-img']");
+    elements.forEach(element => {
+        element.style.display = "none";
+    });
+}
+
+/* Physical Art Button */
+function showPhysical() {
+    const elements = document.querySelectorAll("[id^='physical-img']");
+    elements.forEach(element => {
+        element.style.display = "block";
+    });
+    hideUI();
+    hideVisual();
+}
+function hidePhysical() {
+    const elements = document.querySelectorAll("[id^='physical-img']");
+    elements.forEach(element => {
+        element.style.display = "none";
+    });
+}
